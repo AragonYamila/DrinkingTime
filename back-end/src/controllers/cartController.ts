93f3ce898e967b5d7fdc08245d257d5b410b5949
yamila <aragonyamila@gmail.com>
2023-07-01 11:49:07 -0300

    confirma compra

--- back-end/src/controllers/cartController.ts
+++ back-end/src/controllers/cartController.ts
@@ -3,7 +3,7 @@ import {Router} from "express";
 const router = Router();
 
 router.post("/log", (req, res) => {
-    console.log(req.body);
+    console.log("Compra realizada",req.body);
     res.status(200).json(req.body);
 });
 
